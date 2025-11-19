49483dd21b9746643fb5815c73bbe6b5bf557887
Chhavi Verma <chhaviverma1703@gmail.com>
2025-11-19 20:25:21 +0530

    starablity rating

--- routes/listing.js
+++ routes/listing.js
@@ -8,29 +8,47 @@ const {isLoggedIn, isOwner,validateListing}=require("../middleware.js")
 
 const listingController=require("../controllers/listings.js");
 
-//1.index route
-router.get("/",wrapAsync(listingController.index));
-
+//index and create route:-
+router.route("/")
+.get(wrapAsync(listingController.index))
+.post(isLoggedIn,validateListing, wrapAsync(listingController.createListing));
 
 //4. new route
 router.get("/new",isLoggedIn, wrapAsync(listingController.renderNewForm));
 
-//3.show route
-router.get("/:id" , wrapAsync(listingController.showListing));
 
-//4.(b)create route
-router.post("/",isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+//update,show and delete route:-
+router.route("/:id")
+.put(isLoggedIn,isOwner,validateListing, wrapAsync(listingController.updateListing))
+.get( wrapAsync(listingController.showListing))
+.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
+
+
+
+
+// //1.index route
+// router.get("/",wrapAsync(listingController.index));
+
+
+// //4. new route
+// router.get("/new",isLoggedIn, wrapAsync(listingController.renderNewForm));
+
+//3.show route
+// router.get("/:id" , wrapAsync(listingController.showListing));
+
+// //4.(b)create route
+// router.post("/",isLoggedIn,validateListing, wrapAsync(listingController.createListing));
 
 
 //5.edit route
 router.get("/:id/edit",isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));
 
 
-//5.(b)update route
-router.put("/:id",isLoggedIn,isOwner,validateListing, wrapAsync(listingController.updateListing));
+// //5.(b)update route
+// router.put("/:id",isLoggedIn,isOwner,validateListing, wrapAsync(listingController.updateListing));
 
-//6.DELETE ROUTE
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
+// //6.DELETE ROUTE
+// router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
 
 
 module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -7,20 +7,41 @@ const LocalStrategy=require("passport-local");
 const {saveRedirectUrl}=require("../middleware.js")
 const userController=require("../controllers/users.js")
 
-//1signup
-router.get("/signup",userController.renderSignupForm)
-router.post("/signup",wrapAsync(userController.signupUser));
+//signup
+router.route("/signup")
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signupUser));
 
-//2.login
-router.get("/login",userController.renderLoginForm);
-router.post("/login",
+//login
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(
     saveRedirectUrl,
     passport.authenticate("local",
         {failureRedirect:'/login',
         failureFlash:true}),wrapAsync(userController.loginUser));
 
-
-//3.logout
+//logout
 router.get("/logout",userController.logout);
 
 module.exports=router;
+
+
+
+// //1signup
+// router.get("/signup",userController.renderSignupForm)
+// router.post("/signup",wrapAsync(userController.signupUser));
+
+// //2.login
+// router.get("/login",userController.renderLoginForm);
+// router.post("/login",
+//     saveRedirectUrl,
+//     passport.authenticate("local",
+//         {failureRedirect:'/login',
+//         failureFlash:true}),wrapAsync(userController.loginUser));
+
+
+// //3.logout
+// router.get("/logout",userController.logout);
+
+
